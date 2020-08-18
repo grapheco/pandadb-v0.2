583823114c5758a3612e173ff75eda7dbaf0cf7b
excelwang <wanghuajin999@163.com>
2020-08-18 15:28:04 +0800

    fix docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,5 +1,4 @@
 mvn package -DskipTests
-cd packaging/target/
-tar -xzvf pandadb-0.2-unix.tar.gz
+tar -xzvf packaging/target/pandadb-0.2-unix.tar.gz
 docker build -t pandadb:0.2 .
 docker-compose up -d
