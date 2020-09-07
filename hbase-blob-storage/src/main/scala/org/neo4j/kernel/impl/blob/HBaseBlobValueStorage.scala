8473adb7ccc34af6980a20276a84b4baa5d4435b
lzxddz <lzxddz@qq.com>
2020-09-07 13:51:35 +0800

    delete semicolon

--- hbase-blob-storage/src/main/scala/org/neo4j/kernel/impl/blob/HBaseBlobValueStorage.scala
+++ hbase-blob-storage/src/main/scala/org/neo4j/kernel/impl/blob/HBaseBlobValueStorage.scala
@@ -67,7 +67,7 @@ class HBaseBlobValueStorage extends BlobStorage with Logging {
     val bid = generateId()
     _table.put(_hbaseUtil.buildPut(blob, bid))
     logger.debug(s"saved blob: ${bid.asLiteralString()}")
-    bid;
+    bid
   }
 
   override def load(id: BlobId): Option[Blob] = {
@@ -82,7 +82,7 @@ class HBaseBlobValueStorage extends BlobStorage with Logging {
 
   override def delete(id: BlobId): Unit = {
     _table.delete(_hbaseUtil.buildDelete(id))
-    logger.debug(s"deleted blob: ${id.asLiteralString()}");
+    logger.debug(s"deleted blob: ${id.asLiteralString()}")
   }
 
   override def loadGroup(gid: BlobId): Option[Array[Blob]] = {
